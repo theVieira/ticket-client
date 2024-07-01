0154af6c89cc837f1c6d390b0870ec2258f63ad6
Gabriel <gabrielvieiraguimaraes4@gmail.com>
2024-07-01 16:47:30 -0400

    chore: config cors

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,10 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    cors: {
+      origin: "*",
+      methods: "*",
+    },
+  },
 });
