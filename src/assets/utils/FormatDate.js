3b03519576510b8dfa8249556eff99fea5124633
Gabriel <gabrielvieiraguimaraes4@gmail.com>
2024-07-11 11:10:33 -0400

    chore: replace medium to normal

--- src/assets/utils/FormatDate.js
+++ src/assets/utils/FormatDate.js
@@ -3,6 +3,8 @@ export function format_date(date) {
   const day = newDate.getUTCDate();
   const month = newDate.getUTCMonth();
   const year = newDate.getUTCFullYear();
+  const hours = newDate.getUTCHours();
+  const minutes = newDate.getUTCMinutes();
 
-  return `${day}/${month}/${year}`;
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
 }
