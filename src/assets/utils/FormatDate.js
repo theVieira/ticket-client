2e0077e1343baefddd1381f0b247203d44740b11
Gabriel <gabrielvieiraguimaraes4@gmail.com>
2024-07-12 09:00:27 -0400

    chore: format date ajusted with local

--- src/assets/utils/FormatDate.js
+++ src/assets/utils/FormatDate.js
@@ -1,9 +1,9 @@
 export function format_date(date) {
   const newDate = new Date(date);
   const day = newDate.getUTCDate();
-  const month = newDate.getUTCMonth();
+  const month = newDate.getUTCMonth() + 1;
   const year = newDate.getUTCFullYear();
-  const hours = newDate.getUTCHours();
+  const hours = newDate.getUTCHours() - 4;
   const minutes = newDate.getUTCMinutes();
 
   return `${day}/${month}/${year} ${hours}:${minutes}`;
