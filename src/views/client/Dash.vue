dddea6be0fb1c3da050c31931133f98a530df490
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-08-20 16:29:35 -0400

    chore: add construction page message

--- src/views/client/Dash.vue
+++ src/views/client/Dash.vue
@@ -1,101 +1,101 @@
 <template>
-  <main class="client-dashboard-container">
-    <select name="clientOption" id="clientOption" v-model="clientOption">
+	<main class="client-dashboard-container">
+		<h1>Página em construção</h1>
+		<!-- <select name="clientOption" id="clientOption" v-model="clientOption">
       <option value="" selected disabled>Selecione</option>
       <option value="all">Todos</option>
       <option value="month">Mensal</option>
     </select>
-    <canvas id="chart"></canvas>
-  </main>
+    <canvas id="chart"></canvas> -->
+	</main>
 </template>
 
-<script setup>
-import { FetchAPI } from "@/assets/utils/FetchAPI";
-import { InitializeVars } from "@/assets/utils/InitializeVars";
-import { Chart } from "chart.js/auto";
-import { onMounted, ref, watch } from "vue";
+<!-- <script setup>
+import { FetchAPI } from '@/assets/utils/FetchAPI'
+import { InitializeVars } from '@/assets/utils/InitializeVars'
+import { Chart } from 'chart.js/auto'
+import { onMounted, ref, watch } from 'vue'
 
-const { token } = InitializeVars();
+const { token } = InitializeVars()
 
-const clientOption = ref("all");
+const clientOption = ref('all')
 
 onMounted(async () => {
-  const data = await FetchAPI("/client/list", token);
-  const ctx = document.querySelector("#chart");
+	const data = await FetchAPI('/client/list', token)
+	const ctx = document.querySelector('#chart')
 
-  const datasetData = data.map((client) => {
-    return new Object({
-      label: client.name,
-      data: [client.tickets.length],
-    });
-  });
+	const datasetData = data.map((client) => {
+		return new Object({
+			label: client.name,
+			data: [client.tickets.length],
+		})
+	})
 
-  const chart = new Chart(ctx, {
-    type: "polarArea",
-    data: {
-      labels: ["Clientes"],
-      datasets: datasetData,
-    },
-  });
+	const chart = new Chart(ctx, {
+		type: 'polarArea',
+		data: {
+			labels: ['Clientes'],
+			datasets: datasetData,
+		},
+	})
 
-  watch(clientOption, () => {
-    switch (clientOption.value) {
-      case "all":
-        const allData = data.map((client) => {
-          return new Object({
-            label: client.name,
-            data: [client.tickets.length],
-          });
-        });
+	watch(clientOption, () => {
+		switch (clientOption.value) {
+			case 'all':
+				const allData = data.map((client) => {
+					return new Object({
+						label: client.name,
+						data: [client.tickets.length],
+					})
+				})
 
-        chart.data.datasets = allData;
+				chart.data.datasets = allData
 
-        break;
-      case "month":
-        const nowMonth = new Date().getUTCMonth();
+				break
+			case 'month':
+				const nowMonth = new Date().getUTCMonth()
 
-        const datasetData = data.map((client) => {
-          const monthTickets = client.tickets.filter(
-            (ticket) =>
-              new Date(ticket.createdAt).getUTCMonth() - nowMonth <= 30
-          );
+				const datasetData = data.map((client) => {
+					const monthTickets = client.tickets.filter(
+						(ticket) => new Date(ticket.createdAt).getUTCMonth() - nowMonth <= 30
+					)
 
-          return new Object({
-            label: client.name,
-            data: monthTickets.length,
-          });
-        });
+					return new Object({
+						label: client.name,
+						data: monthTickets.length,
+					})
+				})
 
-        chart.data.datasets = datasetData;
-        chart.update();
+				chart.data.datasets = datasetData
+				chart.update()
 
-        break;
+				break
 
-      case "anual":
-        break;
+			case 'anual':
+				break
 
-      default:
-        console.error("unknow dashboard option");
-        break;
-    }
-  });
-});
-</script>
+			default:
+				console.error('unknow dashboard option')
+				break
+		}
+	})
+})
+</script> -->
 
 <style scoped>
 .client-dashboard-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 3rem;
-  padding: 3rem;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 3rem;
+	padding: 3rem;
 }
 
 #clientOption {
-  background: var(--medium-background);
-  padding: 1rem 2rem;
-  border-radius: 1.2rem;
-  color: var(--light-color);
-  cursor: pointer;
+	background: var(--medium-background);
+	padding: 1rem 2rem;
+	border-radius: 1.2rem;
+	color: var(--light-color);
+	cursor: pointer;
 }
 </style>
