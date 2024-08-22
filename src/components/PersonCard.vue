db8d273f592cab6d9bf1e9e9a28bf3cbadc60029
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-08-22 10:05:24 -0400

    feat: add animation on hover and click for open info

--- src/components/PersonCard.vue
+++ src/components/PersonCard.vue
@@ -1,70 +1,62 @@
 <template>
-  <div class="client-card">
-    <img
-      src="@/assets/icons/zoom.svg"
-      alt="zoom icon"
-      class="zoom"
-      @click="openInfo"
-    />
-    <h4>
-      {{ props.name.toUpperCase() }}
-    </h4>
-    <div class="length-info">
-      <p>
-        <strong>Chamados: {{ tickets.length }}</strong>
-      </p>
-    </div>
-  </div>
+	<div class="client-card" @click="openInfo">
+		<h4>
+			{{ props.name.toUpperCase() }}
+		</h4>
+		<div class="length-info">
+			<p>
+				<strong>Chamados: {{ tickets.length }}</strong>
+			</p>
+		</div>
+	</div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue'
 
-const showInfo = ref(false);
+const showInfo = ref(false)
 
-const emit = defineEmits(["showInfo"]);
+const emit = defineEmits(['showInfo'])
 
 const props = defineProps({
-  name: String,
-  tickets: {
-    type: Array,
-    default: () => [],
-  },
-});
+	name: String,
+	tickets: {
+		type: Array,
+		default: () => [],
+	},
+})
 
 function openInfo() {
-  showInfo.value = !showInfo.value;
-  emit("showInfo");
+	showInfo.value = !showInfo.value
+	emit('showInfo')
 }
 </script>
 
 <style scoped>
 .client-card {
-  width: fit-content;
-  height: fit-content;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  background: var(--medium-background);
-  padding: 5rem 3rem;
-  border-radius: 1.2rem;
-  position: relative;
+	width: fit-content;
+	height: fit-content;
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+	background: var(--medium-background);
+	padding: 5rem 3rem;
+	border-radius: 1.2rem;
+	position: relative;
+	cursor: pointer;
+	transition: 0.3s;
+}
+
+.client-card:hover {
+	filter: brightness(120%);
 }
 
 .client-card h4 {
-  text-align: center;
+	text-align: center;
 }
 
 .length-info {
-  text-align: center;
-  filter: brightness(80%);
-}
-
-.zoom {
-  position: absolute;
-  filter: brightness(150%);
-  top: 1rem;
-  right: 1rem;
-  cursor: pointer;
+	text-align: center;
+	filter: brightness(80%);
 }
 </style>
