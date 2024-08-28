5df311a88c3e5f502692a1349626a63cd67339d8
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-08-28 16:10:04 -0400

    feat: add open tickets on click in tech

--- src/views/tech/Dash.vue
+++ src/views/tech/Dash.vue
@@ -1,46 +1,105 @@
 <template>
 	<main class="dashboard-container">
-		<div class="dataContainer">
-			<select name="dashOption" id="dashOption" v-model="dashOption">
-				<option value="date">{{ dateInput }}</option>
-				<option value="now">Finalizados Hoje</option>
-				<option value="current" selected>Últimos 30 dias</option>
-				<option value="anual">Anual</option>
-				<option value="tech">Técnico</option>
-			</select>
+		<div class="dash-container" v-show="showTickets == false">
+			<div class="dataContainer">
+				<select name="dashOption" id="dashOption" v-model="dashOption">
+					<option value="date">{{ dateInput }}</option>
+					<option value="now">Finalizados Hoje</option>
+					<option value="current" selected>Últimos 30 dias</option>
+					<option value="anual">Anual</option>
+					<option value="tech">Técnico</option>
+				</select>
 
-			<input
-				type="date"
-				name="dataInput"
-				class="dataInput"
-				v-if="dashOption == 'date'"
-				v-model="date"
-			/>
+				<input
+					type="date"
+					name="dataInput"
+					class="dataInput"
+					v-if="dashOption == 'date'"
+					v-model="date"
+				/>
 
-			<input
-				type="button"
-				value="Buscar"
-				class="dataInput"
-				id="findByDateBtn"
-				v-if="dashOption == 'date'"
-			/>
+				<input
+					type="button"
+					value="Buscar"
+					class="dataInput"
+					id="findByDateBtn"
+					v-if="dashOption == 'date'"
+				/>
+			</div>
+
+			<canvas id="chart"></canvas>
 		</div>
 
-		<canvas id="chart"></canvas>
+		<div class="tickets-container" v-if="showTickets">
+			<Ticket
+				v-for="ticket in tickets"
+				:key="ticket.id"
+				:ticket="ticket"
+				:techs="techs"
+				@deleted="_deleteTicket"
+				@progress="_progressTicket"
+				@finished="_finishTicket"
+				@edited="_editTicket"
+				@noted="_notedTicket"
+				class="ticket"
+			/>
+			<h2 class="closeButton" @click="closeTickets">X</h2>
+		</div>
+		<Popup :msg="msg" :type="type" class="popup" v-show="popup" />
 	</main>
 </template>
 
 <script setup>
+import Ticket from '@/components/Ticket.vue'
+import Popup from '@/components/Popup.vue'
 import { FetchAPI } from '@/assets/utils/FetchAPI'
 import { InitializeVars } from '@/assets/utils/InitializeVars'
 import Chart from 'chart.js/auto'
 import { onMounted, ref, watch } from 'vue'
+import {
+	deleteTicket,
+	editTicket,
+	finishedTicket,
+	notedTicket,
+	progressTicket,
+} from '@/assets/utils/TicketActions'
 
 const dashOption = ref('current')
 const dateInput = ref('Selecione a data')
 const date = ref('')
+const showTickets = ref(false)
 
-function mapMonth(month) {
+function _deleteTicket({ id, status }) {
+	deleteTicket(tickets, id, popup, msg, type, status)
+}
+
+function _progressTicket({ id, status }) {
+	progressTicket(tickets, id, popup, msg, type, status)
+}
+
+function _finishTicket({ id, status }) {
+	finishedTicket(tickets, id, popup, msg, type, status, true)
+}
+
+function _editTicket({ id, status, data }) {
+	editTicket(tickets, id, popup, msg, type, data, status)
+}
+
+function _notedTicket({ id, status, data }) {
+	notedTicket(tickets, id, popup, msg, type, data, status)
+}
+
+function closeTickets() {
+	showTickets.value = false
+}
+
+addEventListener('keydown', (ev) => {
+	if (ev.key == 'Escape') {
+		closeTickets()
+	}
+})
+
+function MapMonth(month) {
 	switch (month) {
 		case 0:
 			return 'Janeiro'
@@ -69,7 +128,7 @@ function mapMonth(month) {
 	}
 }
 
-const { token } = InitializeVars()
+const { token, tickets, techs, popup, msg, type } = InitializeVars()
 
 onMounted(async () => {
 	const res = await FetchAPI('/tech/list', token)
@@ -108,7 +167,7 @@ onMounted(async () => {
 	const chart = new Chart(ctx, {
 		type: 'bar',
 		data: {
-			labels: [mapMonth(new Date().getMonth())],
+			labels: [MapMonth(new Date().getMonth())],
 			datasets: datasetData,
 		},
 		options: {
@@ -118,6 +177,7 @@ onMounted(async () => {
 					beginAtZero: true,
 				},
 			},
+			color: '#CECECE',
 		},
 	})
 
@@ -125,20 +185,20 @@ onMounted(async () => {
 		if (dashOption.value == 'date') {
 			document.querySelector('#findByDateBtn').addEventListener('click', (ev) => {
 				const selectedDate = new Date(date.value)
-				const selectedCompare = `${selectedDate.getDate() + 1}-${(
+				const selectedCompare = `${selectedDate.getDate() + 1}/${(
 					selectedDate.getMonth() + 1
 				)
 					.toString()
-					.padStart(2, '0')}-${selectedDate.getFullYear()}`
+					.padStart(2, '0')}/${selectedDate.getFullYear()}`
 
 				const datasetData = res.map((tech) => {
 					const filterTickets = tech.tickets.filter((ticket) => {
 						const ticketDate = new Date(ticket.finished)
-						const ticketCompare = `${ticketDate.getDate()}-${(
+						const ticketCompare = `${ticketDate.getDate()}/${(
 							ticketDate.getMonth() + 1
 						)
 							.toString()
-							.padStart(2, '0')}-${ticketDate.getFullYear()}`
+							.padStart(2, '0')}/${ticketDate.getFullYear()}`
 
 						if (ticketCompare == selectedCompare) {
 							return ticket
@@ -155,9 +215,42 @@ onMounted(async () => {
 
 				chart.data.datasets = datasetData
 				chart.data.labels = [selectedCompare]
+				chart.options.onClick = async function (ev) {
+					const aTech = await FetchAPI('/tech/list', token)
+
+					const i = chart.getElementsAtEventForMode(
+						ev,
+						'nearest',
+						{ intersect: true },
+						true
+					)[0].datasetIndex
+
+					const name = datasetData[i].label
+
+					const filterIam = aTech.filter((tech) => tech.name == name)[0]
+
+					const filterByDate = filterIam.tickets.filter((ticket) => {
+						const ticketDate = new Date(ticket.finished)
+						const ticketCompare = `${ticketDate.getDate()}/${(
+							ticketDate.getMonth() + 1
+						)
+							.toString()
+							.padStart(2, '0')}/${ticketDate.getFullYear()}`
+
+						if (ticketCompare == selectedCompare) {
+							return ticket
+						}
+					})
+
+					tickets.value = techs.value = filterByDate
+					techs.value = res
+					showTickets.value = true
+				}
+
 				chart.update()
 			})
 		}
+
 		if (dashOption.value == 'now') {
 			const datasetData = current.map((tech) => {
 				const filterNowTickets = tech.tickets.filter((ticket) => {
@@ -181,7 +274,7 @@ onMounted(async () => {
 
 			chart.data.datasets = datasetData
 			chart.data.labels = [
-				`Dia ${new Date().getDate()} de ${mapMonth(new Date().getUTCMonth())}`,
+				`Dia ${new Date().getDate()} de ${MapMonth(new Date().getUTCMonth())}`,
 			]
 			chart.update()
 		}
@@ -200,7 +293,7 @@ onMounted(async () => {
 				return obj
 			})
 
-			chart.data.labels = [mapMonth(new Date().getMonth())]
+			chart.data.labels = [MapMonth(new Date().getMonth())]
 			chart.data.datasets = datasetData
 			chart.update()
 		}
@@ -294,5 +387,34 @@ onMounted(async () => {
 	color: var(--light-color);
 	border: none;
 	cursor: pointer;
+}
+
+.dash-container {
+	width: 100%;
+	height: 100%;
+}
+
+.tickets-container {
+	width: 100%;
+}
+
+.ticket {
+	width: 100%;
+}
+
+.closeButton {
+	color: #ff0000d8;
+	cursor: pointer;
+	position: absolute;
+	top: 2rem;
+	font-size: 3rem;
+	left: 20rem;
+}
+
+@media (max-width: 800px) {
+	.closeButton {
+		left: 3rem;
+		z-index: 0;
+	}
 }
 </style>
