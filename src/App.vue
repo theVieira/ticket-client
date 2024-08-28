c899fa04ab493ee8331b412ab096735d654a37cc
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-08-28 16:09:39 -0400

    chore: add z-index to top in navbar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
-  <main class="app-container">
-    <section class="navbar-section" v-if="$route.path != '/'">
-      <Navbar />
-    </section>
-    <section class="app-section">
-      <RouterView />
-    </section>
-  </main>
+	<main class="app-container">
+		<section class="navbar-section" v-if="$route.path != '/'">
+			<Navbar />
+		</section>
+		<section class="app-section">
+			<RouterView />
+		</section>
+	</main>
 </template>
 
 <script setup>
-import Navbar from "@/components/Navbar.vue";
+import Navbar from '@/components/Navbar.vue'
 </script>
 
 <style scoped>
 .app-container {
-  min-height: 100vh;
-  display: flex;
+	min-height: 100vh;
+	display: flex;
 }
 
 .app-section {
-  width: 100%;
-  min-height: 100%;
-  background: var(--light-background);
+	width: 100%;
+	min-height: 100%;
+	background: var(--light-background);
+}
+
+.navbar-section {
+	z-index: 10;
 }
 </style>
