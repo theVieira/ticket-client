d6edb1a62f7a18cc8b9966fa4142cf2fe56d6937
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-08-30 17:36:40 -0400

    refact: click on column for open tickets info

--- src/router/index.js
+++ src/router/index.js
@@ -1,69 +1,69 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Login from "@/views/Login.vue";
-import Home from "@/views/Home.vue";
-import CreateTicket from "@/views/ticket/Create.vue";
-import CreateClient from "@/views/client/Create.vue";
-import Finished from "@/views/ticket/Finished.vue";
-import Clients from "@/views/client/List.vue";
-import Account from "@/views/tech/Account.vue";
-import Progress from "@/views/ticket/Progress.vue";
-import Techs from "@/views/tech/List.vue";
-import CreateTech from "@/views/tech/Create.vue";
-import DashTech from "@/views/tech/Dash.vue";
-import DashClient from "@/views/client/Dash.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '@/views/Login.vue'
+import Home from '@/views/Home.vue'
+import CreateTicket from '@/views/ticket/Create.vue'
+import CreateClient from '@/views/client/Create.vue'
+import Finished from '@/views/ticket/Finished.vue'
+import Clients from '@/views/client/List.vue'
+import Account from '@/views/tech/Account.vue'
+import Progress from '@/views/ticket/Progress.vue'
+import Techs from '@/views/tech/List.vue'
+import CreateTech from '@/views/tech/Create.vue'
+import DashTech from '@/views/tech/Dash.vue'
+import DashClient from '@/views/client/Dash.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      component: Login,
-    },
-    {
-      path: "/home",
-      component: Home,
-    },
-    {
-      path: "/create/ticket",
-      component: CreateTicket,
-    },
-    {
-      path: "/create/client",
-      component: CreateClient,
-    },
-    {
-      path: "/finisheds",
-      component: Finished,
-    },
-    {
-      path: "/list/clients",
-      component: Clients,
-    },
-    {
-      path: "/account",
-      component: Account,
-    },
-    {
-      path: "/progress",
-      component: Progress,
-    },
-    {
-      path: "/manage/tech",
-      component: Techs,
-    },
-    {
-      path: "/create/tech",
-      component: CreateTech,
-    },
-    {
-      path: "/dash/tech",
-      component: DashTech,
-    },
-    {
-      path: "/dash/client",
-      component: DashClient,
-    },
-  ],
-});
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			component: Login,
+		},
+		{
+			path: '/home',
+			component: Home,
+		},
+		{
+			path: '/create/ticket',
+			component: CreateTicket,
+		},
+		{
+			path: '/create/client',
+			component: CreateClient,
+		},
+		{
+			path: '/finisheds',
+			component: Finished,
+		},
+		{
+			path: '/list/clients',
+			component: Clients,
+		},
+		{
+			path: '/account',
+			component: Account,
+		},
+		{
+			path: '/progress',
+			component: Progress,
+		},
+		{
+			path: '/manage/tech',
+			component: Techs,
+		},
+		{
+			path: '/create/tech',
+			component: CreateTech,
+		},
+		{
+			path: '/dash/tech',
+			component: DashTech,
+		},
+		{
+			path: '/dash/client',
+			component: DashClient,
+		},
+	],
+})
 
-export default router;
+export default router
--- src/views/tech/Dash.vue
+++ src/views/tech/Dash.vue
@@ -1,36 +1,29 @@
 <template>
 	<main class="dashboard-container">
-		<div class="dash-container" v-show="showTickets == false">
-			<div class="dataContainer">
-				<select name="dashOption" id="dashOption" v-model="dashOption">
-					<option value="date">{{ dateInput }}</option>
-					<option value="now">Finalizados Hoje</option>
-					<option value="current" selected>Últimos 30 dias</option>
-					<option value="anual">Anual</option>
-					<option value="tech">Técnico</option>
+		<section class="dashboard-section" v-show="!ticketSection">
+			<div class="controls">
+				<select name="option" id="option" v-model="option">
+					<option value="select-date">Selecione a data</option>
+					<option value="today">Finalizados hoje</option>
+					<option value="month">Últimos 30 dias</option>
+					<option value="annual">Anual</option>
+					<option value="tech">Técnicos</option>
 				</select>
 
 				<input
 					type="date"
-					name="dataInput"
-					class="dataInput"
-					v-if="dashOption == 'date'"
+					name="date"
+					id="dateInput"
+					v-if="option == 'select-date'"
+					class="date"
 					v-model="date"
-				/>
-
-				<input
-					type="button"
-					value="Buscar"
-					class="dataInput"
-					id="findByDateBtn"
-					v-if="dashOption == 'date'"
+					@change="find"
 				/>
 			</div>
-
 			<canvas id="chart"></canvas>
-		</div>
-
-		<div class="tickets-container" v-if="showTickets">
+		</section>
+		<section class="tickets-container" v-if="ticketSection">
+			<input type="button" value="X" id="close" @click="closeTicketSection" />
 			<Ticket
 				v-for="ticket in tickets"
 				:key="ticket.id"
@@ -41,21 +34,21 @@
 				@finished="_finishTicket"
 				@edited="_editTicket"
 				@noted="_notedTicket"
+				@focus="ticketFocusFn"
 				class="ticket"
 			/>
-			<h2 class="closeButton" @click="closeTickets">X</h2>
-		</div>
-		<Popup :msg="msg" :type="type" class="popup" v-show="popup" />
+		</section>
+		<Popup v-if="popup" :msg="msg" :type="type" />
 	</main>
 </template>
 
 <script setup>
 import Ticket from '@/components/Ticket.vue'
 import Popup from '@/components/Popup.vue'
-import { FetchAPI } from '@/assets/utils/FetchAPI'
-import { InitializeVars } from '@/assets/utils/InitializeVars'
-import Chart from 'chart.js/auto'
 import { onMounted, ref, watch } from 'vue'
+import { InitializeVars } from '@/assets/utils/InitializeVars'
+import { FetchAPI } from '@/assets/utils/FetchAPI'
+import Chart from 'chart.js/auto'
 import {
 	deleteTicket,
 	editTicket,
@@ -64,11 +57,14 @@ import {
 	progressTicket,
 } from '@/assets/utils/TicketActions'
 
-const dashOption = ref('current')
-const dateInput = ref('Selecione a data')
-const date = ref('')
-const showTickets = ref(false)
+const option = ref('month')
+const date = ref('Selecione a data')
+const ticketSection = ref(false)
+const ticketFocus = ref(false)
 
+const { token, popup, tickets, msg, type, techs } = InitializeVars()
+
+// <Ticket Functions
 function _deleteTicket({ id, status }) {
 	deleteTicket(tickets, id, popup, msg, type, status)
 }
@@ -89,332 +85,370 @@ function _notedTicket({ id, status, data }) {
 	notedTicket(tickets, id, popup, msg, type, data, status)
 }
 
-function closeTickets() {
-	showTickets.value = false
+function closeTicketSection(ev) {
+	if (!ticketFocus.value || ev.type == 'click') {
+		ticketSection.value = false
+		return
+	}
+
+	ticketSection.value = true
+}
+
+function ticketFocusFn({ data }) {
+	ticketFocus.value = data
 }
 
 addEventListener('keydown', (ev) => {
 	if (ev.key == 'Escape') {
-		closeTickets()
+		closeTicketSection()
 	}
 })
+// Ticket Functions />
 
-function MapMonth(month) {
-	switch (month) {
-		case 0:
-			return 'Janeiro'
-		case 1:
-			return 'Fevereiro'
-		case 2:
-			return 'Março'
-		case 3:
-			return 'Abril'
-		case 4:
-			return 'Maio'
-		case 5:
-			return 'Junho'
-		case 6:
-			return 'Julho'
-		case 7:
-			return 'Agosto'
-		case 8:
-			return 'Setembro'
-		case 9:
-			return 'Outubro'
-		case 10:
-			return 'Novembro'
-		case 11:
-			return 'Dezembro'
-	}
+async function find() {
+	const findTechs = await FetchAPI('/tech/list', token)
+	const datasetData = FilterDate(findTechs, date.value)
+	return datasetData
 }
-
-const { token, tickets, techs, popup, msg, type } = InitializeVars()
 
 onMounted(async () => {
 	const res = await FetchAPI('/tech/list', token)
+	techs.value = res
+
 	const ctx = document.querySelector('#chart')
-
-	const current = res.map((tech) => {
-		const now = new Date()
-		const tickets = tech.tickets.map((ticket) => {
-			const ticketDate = new Date(ticket.createdAt)
-
-			if (ticketDate.getUTCDate() > now.getUTCDate() - 30) {
-				return ticket
-			} else {
-				return
-			}
-		})
-
-		tech.tickets = tickets
-		return tech
-	})
-
-	const datasetData = current.map((el) => {
-		const data = new Array()
-		data.push(el.tickets.length)
-
-		const obj = new Object({
-			label: el.name,
-			data,
-			borderWidth: 1,
-			backgroundColor: el.color,
-		})
-
-		return obj
-	})
-
+	const datasetData = FilterMonth(techs.value)
 	const chart = new Chart(ctx, {
 		type: 'bar',
 		data: {
-			labels: [MapMonth(new Date().getMonth())],
+			labels: [MapMonth(new Date().getMonth() + 1)],
 			datasets: datasetData,
 		},
 		options: {
+			color: '#CECECE',
 			animation: true,
-			scales: {
-				y: {
-					beginAtZero: true,
+			layout: {
+				padding: 30,
+			},
+			plugins: {
+				legend: {
+					position: 'bottom',
 				},
 			},
-			color: '#CECECE',
+			responsive: true,
+			aspectRatio: 2 / 1.5,
+			onClick: (ev) => {
+				const name = FindIam(ev, datasetData)
+				tickets.value = datasetData.filter((el) => el.label == name)[0].tickets
+				ticketSection.value = true
+			},
 		},
 	})
 
-	watch(dashOption, () => {
-		if (dashOption.value == 'date') {
-			document.querySelector('#findByDateBtn').addEventListener('click', (ev) => {
-				const selectedDate = new Date(date.value)
-				const selectedCompare = `${selectedDate.getDate() + 1}/${(
-					selectedDate.getMonth() + 1
-				)
-					.toString()
-					.padStart(2, '0')}/${selectedDate.getFullYear()}`
+	function FindIam(ev, dataset) {
+		const i = chart.getElementsAtEventForMode(ev, 'nearest', { intersect: true })[0]
+			.datasetIndex
 
-				const datasetData = res.map((tech) => {
-					const filterTickets = tech.tickets.filter((ticket) => {
-						const ticketDate = new Date(ticket.finished)
-						const ticketCompare = `${ticketDate.getDate()}/${(
-							ticketDate.getMonth() + 1
-						)
-							.toString()
-							.padStart(2, '0')}/${ticketDate.getFullYear()}`
+		return dataset[i].label
+	}
 
-						if (ticketCompare == selectedCompare) {
-							return ticket
-						}
-					})
-
-					return new Object({
-						label: tech.name,
-						borderWidth: 1,
-						backgroundColor: tech.color,
-						data: [filterTickets.length],
-					})
-				})
-
-				chart.data.datasets = datasetData
-				chart.data.labels = [selectedCompare]
-				chart.options.onClick = async function (ev) {
-					const aTech = await FetchAPI('/tech/list', token)
-
-					const i = chart.getElementsAtEventForMode(
-						ev,
-						'nearest',
-						{ intersect: true },
-						true
-					)[0].datasetIndex
-
-					const name = datasetData[i].label
-
-					const filterIam = aTech.filter((tech) => tech.name == name)[0]
-
-					const filterByDate = filterIam.tickets.filter((ticket) => {
-						const ticketDate = new Date(ticket.finished)
-						const ticketCompare = `${ticketDate.getDate()}/${(
-							ticketDate.getMonth() + 1
-						)
-							.toString()
-							.padStart(2, '0')}/${ticketDate.getFullYear()}`
-
-						if (ticketCompare == selectedCompare) {
-							return ticket
-						}
-					})
-
-					tickets.value = techs.value = filterByDate
-					techs.value = res
-					showTickets.value = true
-				}
-
-				chart.update()
-			})
-		}
-
-		if (dashOption.value == 'now') {
-			const datasetData = current.map((tech) => {
-				const filterNowTickets = tech.tickets.filter((ticket) => {
-					const ticketDate = `${new Date(
-						ticket.finished
-					).getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`
-					const now = `${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`
-
-					if (now == ticketDate) {
-						return ticket
+	watch(option, async (val) => {
+		switch (val) {
+			case 'select-date': {
+				watch(date, async () => {
+					const datasetData = await find()
+					chart.data.datasets = datasetData
+					chart.data.labels = [MapMonth(new Date(date.value).getMonth() + 1)]
+					chart.options.onClick = (ev) => {
+						const name = FindIam(ev, datasetData)
+						tickets.value = datasetData.filter((el) => el.label == name)[0].tickets
+						ticketSection.value = true
 					}
+					chart.update()
 				})
+				break
+			}
 
-				return new Object({
-					label: tech.name,
-					borderWidth: 1,
-					backgroundColor: tech.color,
-					data: [filterNowTickets.length],
-				})
-			})
+			case 'today': {
+				const date = new Date()
+				const dateString = `${date.getDate()}/${(date.getMonth() + 1)
+					.toString()
+					.padStart(2, '0')}/${date.getFullYear()}`
 
-			chart.data.datasets = datasetData
-			chart.data.labels = [
-				`Dia ${new Date().getDate()} de ${MapMonth(new Date().getUTCMonth())}`,
-			]
-			chart.update()
-		}
-		if (dashOption.value == 'current') {
-			const datasetData = current.map((el) => {
-				const data = new Array()
-				data.push(el.tickets.length)
+				const datasetData = FilterToday(techs.value)
+				chart.data.datasets = datasetData
+				chart.data.labels = [dateString]
+				chart.options.onClick = (ev) => {
+					const name = FindIam(ev, datasetData)
+					tickets.value = datasetData.filter((el) => el.label == name)[0].tickets
+					ticketSection.value = true
+				}
+				chart.update()
+				break
+			}
 
-				const obj = new Object({
-					label: el.name,
-					data,
-					borderWidth: 1,
-					backgroundColor: el.color,
-				})
+			case 'month': {
+				const datasetData = FilterMonth(techs.value)
+				chart.data.datasets = datasetData
+				chart.data.labels = [MapMonth(new Date().getMonth() + 1)]
+				chart.options.onClick = (ev) => {
+					const name = FindIam(ev, datasetData)
+					tickets.value = datasetData.filter((el) => el.label == name)[0].tickets
+					ticketSection.value = true
+				}
+				chart.update()
+				break
+			}
 
-				return obj
-			})
+			case 'annual': {
+				const datasetData = FilterAnnual(techs.value)
+				chart.data.datasets = datasetData
+				chart.data.labels = [
+					'Janeiro',
+					'Fevereiro',
+					'Março',
+					'Abril',
+					'Maio',
+					'Junho',
+					'Julho',
+					'Agosto',
+					'Setembro',
+					'Outubro',
+					'Novembro',
+					'Dezembro',
+				]
+				chart.options.onClick = (ev, elements) => {
+					const name = FindIam(ev, datasetData)
+					const annualTickets = datasetData.filter((el) => el.label == name)[0]
+						.tickets
 
-			chart.data.labels = [MapMonth(new Date().getMonth())]
-			chart.data.datasets = datasetData
-			chart.update()
-		}
+					if (elements.length) {
+						const element = elements[0]
+						const i = element.index
+						tickets.value = annualTickets[i]
+						ticketSection.value = true
+					}
+				}
+				chart.update()
+				break
+			}
 
-		if (dashOption.value == 'anual') {
-			const datasetData = res.map((tech) => {
-				const months = [[], [], [], [], [], [], [], [], [], [], [], []]
-
-				const newTech = new Object({
-					label: tech.name,
-					data: months,
-					borderWidth: 1,
-					backgroundColor: tech.color,
-				})
-
-				tech.tickets.forEach((ticket) => {
-					const ticketDate = new Date(ticket.createdAt).getUTCMonth()
-					newTech.data[ticketDate]++
-				})
-
-				return newTech
-			})
-
-			chart.data.labels = [
-				'Janeiro',
-				'Fevereiro',
-				'Março',
-				'Abril',
-				'Maio',
-				'Junho',
-				'Julho',
-				'Agosto',
-				'Setembro',
-				'Outubro',
-				'Novembro',
-				'Dezembro',
-			]
-
-			chart.data.datasets = datasetData
-			chart.update()
-		}
-
-		if (dashOption.value == 'tech') {
-			const datasetData = res.map((tech) => {
-				return new Object({
-					label: tech.name,
-					data: [tech.tickets.length],
-					borderWidth: 1,
-					backgroundColor: tech.color,
-				})
-			})
-
-			chart.data.labels = ['Técnicos']
-			chart.data.datasets = datasetData
-
-			chart.update()
+			case 'tech': {
+				const datasetData = FilterTech(techs.value)
+				chart.data.datasets = datasetData
+				chart.data.labels = ['Técnicos']
+				chart.options.onClick = (ev) => {
+					const name = FindIam(ev, datasetData)
+					tickets.value = datasetData.filter((el) => el.label == name)[0].tickets
+					ticketSection.value = true
+				}
+				chart.update()
+			}
 		}
 	})
 })
+
+//	<Filters
+function FilterMonth(techs) {
+	return techs.map((tech) => {
+		const nowDate = new Date()
+		const compareNow = `${nowDate.getMonth()}/${nowDate.getFullYear()}`
+		const filterFinished = tech.tickets.filter((ticket) => ticket.finished)
+
+		const filterMonth = filterFinished.filter((ticket) => {
+			const ticketDate = new Date(ticket.finished)
+			const compareTicket = `${ticketDate.getMonth()}/${ticketDate.getFullYear()}`
+
+			if (compareNow.toString() === compareTicket.toString()) {
+				return ticket
+			}
+		})
+
+		return new Object({
+			label: tech.name,
+			borderWidth: 1,
+			backgroundColor: tech.color,
+			data: [filterMonth.length],
+			tickets: filterMonth,
+		})
+	})
+}
+
+function FilterDate(techs, date) {
+	return techs.map((tech) => {
+		const filterFinished = tech.tickets.filter((ticket) => ticket.finished)
+		const filterTickets = filterFinished.filter((ticket) => {
+			const ticketDate = new Date(ticket.finished)
+			const ticketDateFormat = `${ticketDate.getFullYear()}-${(
+				ticketDate.getMonth() + 1
+			)
+				.toString()
+				.padStart(2, '0')}-${ticketDate.getDate()}`
+
+			if (ticketDateFormat == date) {
+				return ticket
+			}
+		})
+
+		return new Object({
+			label: tech.name,
+			borderWidth: 1,
+			backgroundColor: tech.color,
+			data: [filterTickets.length],
+			tickets: filterTickets,
+		})
+	})
+}
+
+function FilterToday(techs) {
+	const nowDate = new Date()
+	const nowCompare = `${nowDate.getDate()}-${nowDate.getMonth()}-${nowDate.getFullYear()}`
+
+	return techs.map((tech) => {
+		const filterFinished = tech.tickets.filter((ticket) => ticket.finished)
+		const filterToday = filterFinished.filter((ticket) => {
+			const ticketDate = new Date(ticket.finished)
+			const ticketCompare = `${ticketDate.getDate()}-${ticketDate.getMonth()}-${ticketDate.getFullYear()}`
+
+			if (nowCompare === ticketCompare) {
+				return ticket
+			}
+		})
+
+		return new Object({
+			label: tech.name,
+			borderWidth: 1,
+			backgroundColor: tech.color,
+			data: [filterToday.length],
+			tickets: filterToday,
+		})
+	})
+}
+
+function FilterAnnual(techs) {
+	return techs.map((tech) => {
+		const months = [[], [], [], [], [], [], [], [], [], [], [], []]
+
+		const filterFinished = tech.tickets.filter((ticket) => ticket.finished)
+
+		filterFinished.forEach((ticket) => {
+			const ticketDate = new Date(ticket.finished).getMonth()
+			return months[ticketDate].push(ticket)
+		})
+
+		return new Object({
+			label: tech.name,
+			borderWidth: 1,
+			backgroundColor: tech.color,
+			data: months.map((el) => el.length),
+			tickets: months,
+		})
+	})
+}
+
+function FilterTech(techs) {
+	return techs.map((tech) => {
+		const filterFinished = tech.tickets.filter((ticket) => ticket.finished)
+
+		return new Object({
+			label: tech.name,
+			borderWidth: 1,
+			backgroundColor: tech.color,
+			data: [filterFinished.length],
+			tickets: filterFinished,
+		})
+	})
+}
+// Filter />
+
+// <Mapper
+function MapMonth(month) {
+	switch (month) {
+		case 1:
+			return 'Janeiro'
+		case 2:
+			return 'Fevereiro'
+		case 3:
+			return 'Março'
+		case 4:
+			return 'Abril'
+		case 5:
+			return 'Maio'
+		case 6:
+			return 'Junho'
+		case 7:
+			return 'Julho'
+		case 8:
+			return 'Agosto'
+		case 9:
+			return 'Setembro'
+		case 10:
+			return 'Outubro'
+		case 11:
+			return 'Novembro'
+		case 12:
+			return 'Dezembro'
+		default:
+			throw new Error('unknown month')
+	}
+}
+// Mapper />
 </script>
 
 <style scoped>
-.dashboard-container {
-	height: 100%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-	padding: 5rem;
-}
-
-#dashOption {
-	background: var(--medium-background);
-	color: var(--light-color);
-	padding: 1rem 2rem;
-	border-radius: 1.2rem;
-}
-
-.dataContainer {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	gap: 2rem;
-	margin-bottom: 2rem;
-}
-
-.dataInput {
-	border-radius: 1.2rem;
-	padding: 1rem 2rem;
-	background: var(--medium-background);
-	color: var(--light-color);
-	border: none;
-	cursor: pointer;
-}
-
-.dash-container {
-	width: 100%;
-	height: 100%;
-}
-
 .tickets-container {
-	width: 100%;
+	margin-top: 5rem;
 }
 
 .ticket {
 	width: 100%;
 }
 
-.closeButton {
-	color: #ff0000d8;
-	cursor: pointer;
+#close {
 	position: absolute;
 	top: 2rem;
-	font-size: 3rem;
 	left: 20rem;
+	background: none;
+	border: none;
+	color: red;
+	font-size: 3rem;
+	font-weight: 600;
+	cursor: pointer;
+}
+
+.dashboard-section {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	height: 100vh;
+	gap: 3rem;
+}
+
+.controls {
+	display: flex;
+	gap: 2rem;
+}
+
+.controls input,
+select {
+	border: 1px solid var(--light-color);
+	border-radius: 1.2rem;
+	padding: 1rem 2rem;
+	background: var(--medium-background);
+	color: var(--light-color);
+	font-weight: 600;
+	cursor: pointer;
+}
+
+#chart {
+	max-width: 100%;
+	max-height: 80%;
 }
 
 @media (max-width: 800px) {
-	.closeButton {
+	#close {
 		left: 3rem;
-		z-index: 0;
 	}
 }
 </style>
