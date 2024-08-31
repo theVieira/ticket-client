89eb28526e3e80acbdcd401b9492bd35ce649f64
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-08-31 11:09:04 -0400

    feat: config server

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	server: {
+		cors: {
+			methods: '*',
+			origin: '*',
+		},
+		host: '0.0.0.0',
+		port: '5050',
+		origin: '127.0.0.1:5050',
+	},
+})
