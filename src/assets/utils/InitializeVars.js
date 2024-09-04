e42bee1760194e5f9291b146022d19036ecf94db
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-09-04 17:16:11 -0400

    feat: implements refresh token every 12h

--- src/assets/utils/InitializeVars.js
+++ src/assets/utils/InitializeVars.js
@@ -1,37 +1,39 @@
-import { ref } from "vue";
+import { ref } from 'vue'
 
 export function InitializeVars() {
-  const admin = localStorage.getItem("admin");
-  const create_ticket = localStorage.getItem("create_ticket");
-  const delete_ticket = localStorage.getItem("delete_ticket");
-  const token = localStorage.getItem("token");
-  const techName = localStorage.getItem("techName");
-  const techColor = localStorage.getItem("techColor");
-  const popup = ref(false);
-  const msg = ref("");
-  const type = ref("");
-  const tickets = ref([]);
-  const clients = ref([]);
-  const techs = ref([]);
-  const total = ref(0);
-  const clientName = ref("");
-  const order = ref("all");
+	const admin = localStorage.getItem('admin')
+	const create_ticket = localStorage.getItem('create_ticket')
+	const delete_ticket = localStorage.getItem('delete_ticket')
+	const token = localStorage.getItem('token')
+	const techName = localStorage.getItem('techName')
+	const techColor = localStorage.getItem('techColor')
+	const expires = localStorage.getItem('expiresToken')
+	const popup = ref(false)
+	const msg = ref('')
+	const type = ref('')
+	const tickets = ref([])
+	const clients = ref([])
+	const techs = ref([])
+	const total = ref(0)
+	const clientName = ref('')
+	const order = ref('all')
 
-  return {
-    token,
-    techName,
-    techColor,
-    popup,
-    msg,
-    type,
-    tickets,
-    clients,
-    techs,
-    total,
-    clientName,
-    order,
-    admin,
-    create_ticket,
-    delete_ticket,
-  };
+	return {
+		token,
+		techName,
+		techColor,
+		popup,
+		msg,
+		type,
+		tickets,
+		clients,
+		techs,
+		total,
+		clientName,
+		order,
+		admin,
+		create_ticket,
+		delete_ticket,
+		expires,
+	}
 }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,148 +1,154 @@
 <template>
-  <div class="login-container">
-    <form action="post" @submit.prevent="login">
-      <h1>LOGIN</h1>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        maxlength="50"
-        placeholder="Digite seu nome"
-        v-model="name"
-        class="form-input"
-      />
-      <input
-        type="password"
-        name="password"
-        id="password"
-        maxlength="50"
-        placeholder="Digite sua senha"
-        v-model="password"
-        class="form-input"
-      />
-      <input type="submit" value="Entrar" />
-    </form>
-    <Popup class="popup" v-if="popup" :msg="msg" :type="type" />
-  </div>
+	<div class="login-container">
+		<form action="post" @submit.prevent="login">
+			<h1>LOGIN</h1>
+			<input
+				type="text"
+				name="name"
+				id="name"
+				maxlength="50"
+				placeholder="Digite seu nome"
+				v-model="name"
+				class="form-input"
+			/>
+			<input
+				type="password"
+				name="password"
+				id="password"
+				maxlength="50"
+				placeholder="Digite sua senha"
+				v-model="password"
+				class="form-input"
+			/>
+			<input type="submit" value="Entrar" />
+		</form>
+		<Popup class="popup" v-if="popup" :msg="msg" :type="type" />
+	</div>
 </template>
 
 <script setup>
-import Popup from "../components/Popup.vue";
-import { onBeforeMount, ref } from "vue";
-import { baseUrl } from "../../conf";
-import { SetTitle } from "@/assets/utils/SetTitle";
-import { InitializeVars } from "@/assets/utils/InitializeVars";
-import { ShowPopup } from "@/assets/utils/ShowPopup";
-import router from "@/router";
+import Popup from '../components/Popup.vue'
+import { onBeforeMount, ref } from 'vue'
+import { baseUrl } from '../../conf'
+import { SetTitle } from '@/assets/utils/SetTitle'
+import { InitializeVars } from '@/assets/utils/InitializeVars'
+import { ShowPopup } from '@/assets/utils/ShowPopup'
+import router from '@/router'
 
-SetTitle("Login");
+SetTitle('Login')
 
-const { msg, type, popup } = InitializeVars();
+const { msg, type, popup } = InitializeVars()
 
-const name = ref("");
-const password = ref("");
+const name = ref('')
+const password = ref('')
 
 onBeforeMount(() => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    router.push("/home");
-  }
-});
+	const token = localStorage.getItem('token')
+	const expires = localStorage.getItem('expiresToken')
+
+	if (expires < Date.now() - 1000 * 60 * 60 * 12 && token) {
+		router.push('/home')
+	} else {
+		localStorage.clear()
+		router.push('/')
+	}
+})
 
 async function login() {
-  const res = await fetch(baseUrl + "/tech/auth", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: name.value,
-      password: password.value,
-    }),
-  });
+	const res = await fetch(baseUrl + '/tech/auth', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			name: name.value,
+			password: password.value,
+		}),
+	})
 
-  name.value = "";
-  password.value = "";
+	name.value = ''
+	password.value = ''
 
-  const data = await res.json();
+	const data = await res.json()
 
-  popup.value = true;
-  setTimeout(() => {
-    popup.value = false;
-  }, 1000 * 3);
+	popup.value = true
+	setTimeout(() => {
+		popup.value = false
+	}, 1000 * 3)
 
-  if (res.status === 200) {
-    ShowPopup(popup, msg, type, "Logado com sucesso", "success");
+	if (res.status === 200) {
+		ShowPopup(popup, msg, type, 'Logado com sucesso', 'success')
 
-    localStorage.setItem("token", data.token);
-    localStorage.setItem("techName", data.name);
-    localStorage.setItem("admin", data.admin);
-    localStorage.setItem("create_ticket", data.create_ticket);
-    localStorage.setItem("delete_ticket", data.delete_ticket);
-    localStorage.setItem("color", data.color);
-    router.push("/home");
-  } else {
-    ShowPopup(popup, msg, type, "Ops! Ocorreu algum erro!", "error");
+		localStorage.setItem('token', data.token)
+		localStorage.setItem('techName', data.name)
+		localStorage.setItem('admin', data.admin)
+		localStorage.setItem('create_ticket', data.create_ticket)
+		localStorage.setItem('delete_ticket', data.delete_ticket)
+		localStorage.setItem('color', data.color)
+		localStorage.setItem('expiresToken', Date.now())
+		router.push('/home')
+	} else {
+		ShowPopup(popup, msg, type, 'Ops! Ocorreu algum erro!', 'error')
 
-    console.error(data);
+		console.error(data)
 
-    const inputs = document.querySelectorAll(".form-input");
-    inputs.forEach((input) => {
-      input.style.border = "2px solid #d34242";
-      setTimeout(() => {
-        input.style.border = "";
-      }, 1000 * 3);
-    });
-  }
+		const inputs = document.querySelectorAll('.form-input')
+		inputs.forEach((input) => {
+			input.style.border = '2px solid #d34242'
+			setTimeout(() => {
+				input.style.border = ''
+			}, 1000 * 3)
+		})
+	}
 }
 </script>
 
 <style scoped>
 .login-container {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: var(--dark-background);
-  color: var(--light-color);
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background: var(--dark-background);
+	color: var(--light-color);
 }
 
 .login-container form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 2rem;
-  padding: 5rem;
-  border-radius: 1.2rem;
-  background: #5a58e6;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 2rem;
+	padding: 5rem;
+	border-radius: 1.2rem;
+	background: #5a58e6;
 }
 
 .login-container form input {
-  border: none;
-  border-radius: 1.2rem;
-  padding: 1rem 2rem;
-  background: var(--light-color);
+	border: none;
+	border-radius: 1.2rem;
+	padding: 1rem 2rem;
+	background: var(--light-color);
 }
 
-.login-container form input[type="submit"] {
-  font-weight: 600;
-  width: 100%;
-  background: #565cb3aa;
-  color: var(--light-color);
-  font-size: 1.5rem;
-  cursor: pointer;
-  filter: brightness(85%);
-  transition: 0.3s;
+.login-container form input[type='submit'] {
+	font-weight: 600;
+	width: 100%;
+	background: #565cb3aa;
+	color: var(--light-color);
+	font-size: 1.5rem;
+	cursor: pointer;
+	filter: brightness(85%);
+	transition: 0.3s;
 }
 
-.login-container form input[type="submit"]:hover {
-  filter: brightness(100%);
+.login-container form input[type='submit']:hover {
+	filter: brightness(100%);
 }
 
 .popup {
-  position: absolute;
-  top: 3rem;
+	position: absolute;
+	top: 3rem;
 }
 </style>
