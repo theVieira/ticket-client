b94114a2ecc72713d727b7707d0fcdbe15dc54fe
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-09-12 15:51:25 -0400

    refact error

--- src/assets/utils/InitializeVars.js
+++ src/assets/utils/InitializeVars.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ref } from 'vue'
 
 export function InitializeVars() {
@@ -38,42 +37,3 @@ export function InitializeVars() {
 		expires,
 	}
 }
-=======
-import { ref } from 'vue'
-
-export function InitializeVars() {
-  const admin = localStorage.getItem('admin')
-  const create_ticket = localStorage.getItem('create_ticket')
-  const delete_ticket = localStorage.getItem('delete_ticket')
-  const token = localStorage.getItem('token')
-  const techName = localStorage.getItem('techName')
-  const techColor = localStorage.getItem('techColor')
-  const popup = ref(false)
-  const msg = ref('')
-  const type = ref('')
-  const tickets = ref([])
-  const clients = ref([])
-  const techs = ref([])
-  const total = ref(0)
-  const clientName = ref('')
-  const order = ref('all')
-
-  return {
-    token,
-    techName,
-    techColor,
-    popup,
-    msg,
-    type,
-    tickets,
-    clients,
-    techs,
-    total,
-    clientName,
-    order,
-    admin,
-    create_ticket,
-    delete_ticket,
-  }
-}
->>>>>>> development
